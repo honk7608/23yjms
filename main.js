67b16431e6c2db47c8a6210ab24b1448bc045eea
honk7608 <honk7608@users.noreply.github.com>
2023-04-09 16:15:42 +0900

    파일 가져오기 방식 수정

--- main.js
+++ main.js
@@ -17,30 +17,45 @@ app.use(cookieParser())
 
 /** 
      * 주어진 데이터로 요청에 응답해주는 함수
-     * @param {Response} res 
-     * @param {string} htmlRoute 
-     * @param {{code: string, content: string}[]} replacement 
+     * @param {Response} res Response 변수
+     * @param {string} routeName 경로명 (ex: PageData/home/home.html -> home)
+     * @param {{code: string, content: string|[]}[]} replacement Html 내용을 replace할 내용
      */
-let EndWithRespond = function (res, htmlRoute=null, replacement) {
+let EndWithRespond = function (res, routeName=null, replacement) {
     var fs = require('fs')
     var baseData = String(fs.readFileSync('./PageData/base.html', 'utf-8'))
-    var respondData = String(fs.readFileSync(htmlRoute, 'utf-8'))
+    var respondData = String(fs.readFileSync(`./PageData/${routeName}/${routeName}.html`, 'utf-8'))
 
     if (replacement) {
-        replacement.push({code: 'main_content', content: respondData})
+        replacement = [{code: 'main_content', content: respondData}].concat(replacement)
     } else {
         replacement = [{code: 'main_content', content: respondData}]
     }
+
+    // css replacement 존재여부 확인
+    var cssExist = false
+    for (const key in replacement) {
+        if (replacement[key].code == 'css_additional') {
+            var cssExist = true
+            replacement[key].content.push(`css/${routeName}/${routeName}.css`)
+            break
+        }
+    }
+    if (!cssExist) {
+        replacement.push({code: 'css_additional', content: [`css/${routeName}/${routeName}.css`]})
+    }
+
+    // 대체 작업
     for (const key in replacement) {
         if (replacement[key].code == 'css_additional') {
             cssReplList = replacement[key].content
-            cssReplText = ''
+            cssReplText = ``
             for (i = 0; i < cssReplList.length; i++) {
-                cssReplText += `<link rel="stylesheet" href="${cssReplList[i]}">`
+                cssReplText += `<link rel="stylesheet" href="${cssReplList[i]}">\n`
             }
             replacement[key].content = cssReplText
         }
-        // console.log(key, replacement[key])
+
         baseData = baseData.replace(`<!-- {${replacement[key].code}} -->`, replacement[key].content)
     }
 
@@ -58,17 +73,26 @@ app.get('/', async function (req, res) {
     const timetable = new Timetable();
 
     async function getTimetable() {
-        await timetable.init({ cache: 1000 * 60 * 60 })
-        await timetable.setSchool(49930)
+        while(true) {
+            try {
+                await timetable.init({ cache: 1000 * 60 * 60 })
+                await timetable.setSchool(49930)
+            } catch {
+                continue
+            }
+            break
+        }
         result = await timetable.getTimetable()
+        return result
     }
 
-    getTimetable()
-    EndWithRespond(res, './PageData/home/home.html', [
+    tableData = await getTimetable()
+
+    EndWithRespond(res, 'home', [
         {
-            code: 'css_additional', 
-            content: ['css/home/home.css']
-        },
+            code: 'timetableList',
+            content: JSON.stringify(tableData[3][11][0])
+        }
     ]
     )
 })
