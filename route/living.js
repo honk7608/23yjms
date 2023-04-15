a79c14f205e8141f1ad0bd338e6c33a0ab68aff7
honk7608 <honk7608@users.noreply.github.com>
2023-04-15 15:04:18 +0900

    시간표 페이지 초안 (시간표 디자인 제외)

--- route/living.js
+++ route/living.js
@@ -2,9 +2,51 @@ var express = require('express');
 var asyncify = require('express-asyncify');
 var router = asyncify(express.Router());
 let EndWithRespond = require('../SubModule/ResFunc.js')
+var url = require('url');
 
 router.get('/timetable', async function (req, res) {
-    EndWithRespond(res, 'live;timetable')
+    var requrl = req.url;
+    var queryData = url.parse(requrl, true).query;
+
+    var ScGrade = Number(queryData.grade)
+    var ScClass = Number(queryData.class)
+
+    if (ScGrade && ScClass) {
+        if (ScGrade != 3 && ScClass > 10) {
+            return res.redirect('/living/timetable')
+        } else {
+            const Timetable = require('comcigan-parser');
+            const timetable = new Timetable();
+        
+            async function getTimetable() {
+                while(true) {
+                    try {
+                        await timetable.init({ cache: 1000 * 60 * 60 })
+                        await timetable.setSchool(49930)
+                    } catch {
+                        continue
+                    }
+                    break
+                }
+                result = await timetable.getTimetable()
+                return result
+            }
+            
+            tableData = await getTimetable()          
+            tableData = tableData[ScGrade][ScClass]
+        }
+    } else {
+        tableData = null
+    }
+        
+    EndWithRespond(
+        res, 
+        'live;timetable',
+        [{
+            code: 'timetable_variable',
+            content: JSON.stringify(tableData)
+        }]
+        )
 })
 
 router.get('/meal', async function (req, res) {
