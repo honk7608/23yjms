442be1aa6ab3676d350f12fbff9f0d7965a7e8d6
honk7608 <honk7608@users.noreply.github.com>
2023-04-26 00:11:13 +0900

    회원가입/로그인 DB 적용

--- route/member.js
+++ route/member.js
@@ -18,15 +18,25 @@ router.get('/login', async function (req, res) {
 });
 
 router.post('/login', async function (req, res) {
-    if (req.body.id == '31121' && req.body.pw == '11111111') {
+    const connection = await mysql.createConnection(req.dbOption);
+    const [Users, fields] = await connection.execute(
+        `SELECT * FROM user
+        WHERE user.id = ${req.body.id}
+        LIMIT 1;`
+    )
+
+    if(Users.length == 0) { //해당 유저 미존재
+        res.redirect('/member/login')
+    } else if (Users[0].pw != req.body.pw) { //잘못된 PW
+        res.redirect('/member/login')
+    } else {
         req.session.member = {
             isLogged: true,
             id: req.body.id
         }
         res.redirect(req.session.lastUrl)
-    } else {
-        res.redirect('/member/login')
     }
+    connection.end()
 });
 
 router.post('/logout', async function (req, res) {
@@ -43,13 +53,34 @@ router.get('/me', async function (req, res) {
 });
 
 router.get('/sign-up', async function (req, res) {
-    EndWithRespond(req, res, 'mem;signUp')
+    if (req.session.member.isLogged) {
+        if(req.session.lastUrl == '/member/sign-up') {res.redirect('/')}
+        else {res.redirect(req.session.lastUrl)}
+    }
+    else {EndWithRespond(req, res, 'mem;signUp')}    
 });
 
 router.post('/sign-up', async function (req, res) {
-    const connection = mysql.createConnection(req.dbOption);
+    const connection = await mysql.createConnection(req.dbOption);
+    if(req.body.id != Math.round(Number(req.body.id))) {return res.redirect()}
+
+    const [Users, fields] = await connection.execute(
+        `SELECT * FROM user
+        WHERE user.id = ${req.body.id}
+        LIMIT 1;`
+    )
+
+    if(Users.length != 0) {res.redirect('/member/sign-up')}
+    else {
+        await connection.execute(
+            `INSERT INTO user (id, pw, permission, name)
+            VALUES (${req.body.id}, '${req.body.pw}', 0, '${req.body.name}')`
+        )
+    }
 
     res.redirect('/member/login')
+
+    connection.end()
 });
 
 module.exports = router;
