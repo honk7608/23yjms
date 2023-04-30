7f41ea4ee293bdc13ce6dbf17cc7c7ab12b26291
honk7608 <honk7608@users.noreply.github.com>
2023-04-30 23:40:03 +0900

    페이지 기능 추가 및 코드 가독성 개선

--- route/community.js
+++ route/community.js
@@ -9,30 +9,39 @@ const url = require('url');
  * 한 게시판의 지정 범위 내의 글을 가져오는 함수
  * @param {{}} dbOption MySql Connection 옵션
  * @param {String} boardName 게시판의 MySql이름
- * @param {Number} startID 데이터의 시작 범위
+ * @param {Number} startIndex 데이터의 시작 범위
  * @param {Number} count 데이터의 최대 개수
  * @returns {{author_id: Number, author_name: String, createdTime: Date, id: Number, title: String}[]} 게시물 리스트
  */
-async function getBoardArticles(dbOption, boardName, startID=null, count=10) {
+async function getBoardArticles(dbOption, boardName, startIndex=null, count=10) {
+    queryText = 
+`SELECT ${boardName}_board.id, title, createdTime, user.name AS author_name, author_id 
+FROM ${boardName}_board 
+LEFT JOIN user ON ${boardName}_board.author_id = user.id 
+WHERE is_enabled = 1
+ORDER BY ${boardName}_board.id DESC;`
+
     const connection = await mysql.createConnection(dbOption);
 
-    if(startID != null) {
-        additionalQuery = `WHERE ${boardName}_board.id <= ${startID}`
-    } else {
-        additionalQuery = ``
-    }
-
     // 글 모두 가져오기
-    const [Articles, fields] = await connection.execute(
-        `SELECT ${boardName}_board.id, title, createdTime, user.name AS author_name, author_id 
-        FROM ${boardName}_board 
-        LEFT JOIN user ON ${boardName}_board.author_id = user.id 
-        ORDER BY ${boardName}_board.id DESC
-        ${additionalQuery}
-        LIMIT ${count};`);
+    const [Articles, fields] = await connection.execute(queryText);
     
     connection.end()
-    return Articles 
+
+    for(const key in Articles) {
+        if(key != Number(key)) {continue}
+        Articles[key].AllIndex = Articles.length - key
+    }
+
+    if(startIndex == null) {targetArticles = Articles}
+    else {
+        targetArticles = []
+        for(i = startIndex; i < Number(startIndex) + Number(count) && i <= Articles.length; i++) {
+            targetArticles.push(Articles[i - 1])
+            Articles[i - 1].AllIndex
+        }
+    }
+    return targetArticles 
 }
 
 /**
@@ -83,27 +92,32 @@ router.get('/board', async function (req, res) {
     const requrl = req.url;
     const queryData = url.parse(requrl, true).query;
     const boardName = queryData.boardName
-    startArticleID = queryData.startID
-    if(!startArticleID) {startArticleID = null}
-    
-    var Articles = await getBoardArticles(req.dbOption, boardName, startArticleID)
+    targetPage = queryData.page
+    if(!targetPage) {targetPage = 1}
 
     existingBoard = false
     for(const oneboardName in categoryData) {
         if (boardName == oneboardName) {existingBoard = true; break;}
     }
+    if(!existingBoard) {return res.redirect(req.session.lastUrl)}
 
-    if(!existingBoard) {res.redirect('/')}
-    else{
-        EndWithRespond(req, res, 'com;board', [
-            {code: 'pageTitle', content: categoryData[boardName].title},
-            {code: 'pageDescribe', content: categoryData[boardName].describe},
-            {code: 'articles', content: JSON.stringify(Articles)},
-            {code: 'memberID', content: req.session.member.id},
-            {code: 'boardName1', content: boardName},
-            {code: 'boardName2', content: boardName}
-        ])
-    }
+    var Articles = await getBoardArticles(req.dbOption, boardName, targetPage * 10 - 9)
+
+    const lastArticle = await getBoardArticles(req.dbOption, boardName, 1, 1)
+    maxPage = (lastArticle[0].AllIndex - (lastArticle[0].AllIndex % 10)) / 10
+    if(lastArticle[0].AllIndex % 10 != 0) {maxPage += 1}
+
+    EndWithRespond(req, res, 'com;board', [
+        {code: 'pageTitle', content: categoryData[boardName].title},
+        {code: 'pageDescribe', content: categoryData[boardName].describe},
+        {code: 'articles', content: JSON.stringify(Articles)},
+        {code: 'memberID', content: req.session.member.id},
+        {code: 'boardName1', content: boardName},
+        {code: 'boardName2', content: boardName},
+        {code: 'boardName3', content: boardName},
+        {code: 'max_page1', content: maxPage},
+        {code: 'max_page2', content: maxPage}
+    ])
 })
 
 router.get('/viewArticle', async function (req, res) {
@@ -117,74 +131,74 @@ router.get('/viewArticle', async function (req, res) {
         if (boardName == oneboardName) {existingBoard = true; break;}
     }
 
-    if(!existingBoard) {res.redirect(req.session.lastUrl)}
-    else {       
-        const article = await getOneArticle(req.dbOption, boardName, articleID)
-        var displayDateText = `${String(article.createdTime.getFullYear()).slice(-2)}.${('0' + String(article.createdTime.getMonth() + 1)).slice(-2)}.${String('0' + String(article.createdTime.getDate())).slice(-2)}`
+    if(!existingBoard) {return res.redirect(req.session.lastUrl)}
 
-        // LineChange
-        displayContent = article.content.split('\r\n').join('<br>')
+    const article = await getOneArticle(req.dbOption, boardName, articleID)
 
-        // **
-        BoldList = displayContent.split('**')
-        for(i = 0; i < BoldList.length; i++) {
-            if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `**${BoldList[i]}`; continue}
-            if(i % 2 == 0) {continue}
-            BoldList[i] = `<b>${BoldList[i]}</b>`
-        }
-        displayContent = BoldList.join('')
+    var displayDateText = `${String(article.createdTime.getFullYear()).slice(-2)}.${('0' + String(article.createdTime.getMonth() + 1)).slice(-2)}.${String('0' + String(article.createdTime.getDate())).slice(-2)}`
 
-        // $$
-        ItalicList = displayContent.split('$$')
-        for(i = 0; i < ItalicList.length; i++) {
-            if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `$$${BoldList[i]}`; continue}
-            if(i % 2 == 0) {continue}
-            ItalicList[i] = `<i>${ItalicList[i]}</i>`
-        }
-        displayContent = ItalicList.join('')
+    // LineChange
+    displayContent = article.content.split('\r\n').join('<br>')
 
-        // --
-        LineThroughList = displayContent.split('--')
-        for(i = 0; i < LineThroughList.length; i++) {
-            if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `--${BoldList[i]}`; continue}
-            if(i % 2 == 0) {continue}
-            LineThroughList[i] = `<span style="text-decoration: line-through;">${LineThroughList[i]}</span>`
-        }
-        displayContent = LineThroughList.join('')
-
-        // __
-        UnderLineList = displayContent.split('__')
-        for(i = 0; i < UnderLineList.length; i++) {
-            if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `__${BoldList[i]}`; continue}
-            if(i % 2 == 0) {continue}
-            UnderLineList[i] = `<span style="text-decoration: underline;">${UnderLineList[i]}</span>`
-        }
-        displayContent = UnderLineList.join('')
-
-        // @@
-        ColorAccentList = displayContent.split('@@')
-        for(i = 0; i < ColorAccentList.length; i++) {
-            if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `@@${BoldList[i]}`; continue}
-            if(i % 2 == 0) {continue}
-            ColorAccentList[i] = `<span style="color: var(--accent-color);">${ColorAccentList[i]}</span>`
-        }
-        displayContent = ColorAccentList.join('')
-        
-        if(article.author_id == req.session.member.id) {AdditionalClass = ''}
-        else {AdditionalClass = ' NotMe'}
-
-        EndWithRespond(req, res, 'com;viewArticle', [
-            {code: 'articleData', content: displayContent},
-            {code: 'articleID', content: article.id},
-            {code: 'boardName1', content: boardName},
-            {code: 'boardName2', content: boardName},
-            {code: 'title', content: article.title},
-            {code: 'createdDate', content: displayDateText},
-            {code: 'boardStringName', content: categoryData[boardName].title},
-            {code: 'author_name', content: article.author_name},
-            {code: 'ButtonsDivAddClass', content: AdditionalClass}
-        ])
+    // **
+    BoldList = displayContent.split('**')
+    for(i = 0; i < BoldList.length; i++) {
+        if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `**${BoldList[i]}`; continue}
+        if(i % 2 == 0) {continue}
+        BoldList[i] = `<b>${BoldList[i]}</b>`
     }
+    displayContent = BoldList.join('')
+
+    // $$
+    ItalicList = displayContent.split('$$')
+    for(i = 0; i < ItalicList.length; i++) {
+        if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `$$${BoldList[i]}`; continue}
+        if(i % 2 == 0) {continue}
+        ItalicList[i] = `<i>${ItalicList[i]}</i>`
+    }
+    displayContent = ItalicList.join('')
+
+    // --
+    LineThroughList = displayContent.split('--')
+    for(i = 0; i < LineThroughList.length; i++) {
+        if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `--${BoldList[i]}`; continue}
+        if(i % 2 == 0) {continue}
+        LineThroughList[i] = `<span style="text-decoration: line-through;">${LineThroughList[i]}</span>`
+    }
+    displayContent = LineThroughList.join('')
+
+    // __
+    UnderLineList = displayContent.split('__')
+    for(i = 0; i < UnderLineList.length; i++) {
+        if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `__${BoldList[i]}`; continue}
+        if(i % 2 == 0) {continue}
+        UnderLineList[i] = `<span style="text-decoration: underline;">${UnderLineList[i]}</span>`
+    }
+    displayContent = UnderLineList.join('')
+
+    // @@
+    ColorAccentList = displayContent.split('@@')
+    for(i = 0; i < ColorAccentList.length; i++) {
+        if(BoldList.length % 2 == 0 && i == BoldList.length - 1) {BoldList[i] = `@@${BoldList[i]}`; continue}
+        if(i % 2 == 0) {continue}
+        ColorAccentList[i] = `<span style="color: var(--accent-color);">${ColorAccentList[i]}</span>`
+    }
+    displayContent = ColorAccentList.join('')
+    
+    if(article.author_id == req.session.member.id) {AdditionalClass = ''}
+    else {AdditionalClass = ' NotMe'}
+
+    EndWithRespond(req, res, 'com;viewArticle', [
+        {code: 'articleData', content: displayContent},
+        {code: 'articleID', content: article.id},
+        {code: 'boardName1', content: boardName},
+        {code: 'boardName2', content: boardName},
+        {code: 'title', content: article.title},
+        {code: 'createdDate', content: displayDateText},
+        {code: 'boardStringName', content: categoryData[boardName].title},
+        {code: 'author_name', content: article.author_name},
+        {code: 'ButtonsDivAddClass', content: AdditionalClass}
+    ])
 })
 
 router.get('/writeArticle', async function (req, res) {
@@ -247,7 +261,7 @@ router.post('/writeArticle', async function (req, res) {
     if(!articleID) {
         Article.author_id = req.session.member.id
 
-        lastArticle = await getBoardArticles(req.dbOption, boardName, null, 1)
+        lastArticle = await getBoardArticles(req.dbOption, boardName, 1, 1)
         if(lastArticle.length == 0) {Article.id = 1}
         else {Article.id = lastArticle[0].id + 1}
 
