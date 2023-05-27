94756175102db514814ade31dcf825f0662dea91
honk7608 <honk7608@gmail.com>
2023-05-27 12:15:23 +0900

    학년 별 표 파일 생성

--- route/exam.js
+++ route/exam.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var asyncify = require('express-asyncify');
 var router = asyncify(express.Router());
-const EndWithRespond = require('../SubModule/ResFunc.js')
+const EndWithRespond = require('../SubModule/ResFunc.js');
+const url = require('url');
 
 router.get('/eval-list', async function (req, res) {
-    EndWithRespond(req, res, 'exam;list')
+    const requrl = req.url;
+    const queryData = url.parse(requrl, true).query;
+
+    var ScGrade = Number(queryData.grade)
+
+    if(!ScGrade) {
+        tableData = ''
+    } else {
+        const fs = require('fs')
+        if(!(ScGrade == 1 || ScGrade == 2 || ScGrade == 3)) {res.redirect('/exam/eval-list')}
+        tableData = fs.readFileSync(`${req.FileBaseRoot}/PageData/exam;list/exam;tableGrade${ScGrade}.html`)
+    }
+
+    EndWithRespond(req, res, 'exam;list', [{code: 'table_data', content: String(tableData)}])
 })
 
 router.get('/mid-final-exam', async function (req, res) {
