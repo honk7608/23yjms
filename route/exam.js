91b9de8e279b56a654761ef2d55f2e15561ad2db
honk7608 <69580743+honk7608@users.noreply.github.com>
2023-06-16 15:56:06 +0900

    MidFinal 루트 수정

--- route/exam.js
+++ route/exam.js
@@ -41,7 +41,39 @@ router.get('/eval-list', async function (req, res) {
 })
 
 router.get('/mid-final-exam', async function (req, res) {
-    EndWithRespond(req, res, 'exam;midfinal')
+    const requrl = req.url;
+    const queryData = url.parse(requrl, true).query;
+    var ScGrade = Number(queryData.grade)
+
+    var needCookie = String(queryData.cookie) || 'off'
+    
+    if(req.cookies) {
+        var cookieData = req.cookies.defaultExamGrade || null
+    } else {
+        var cookieData = null
+    }
+
+    if(ScGrade && needCookie == 'on') { // 쿠키 업데이트
+        if(cookieData == null) {res.cookie('defaultExamGrade', ScGrade, {maxAge: 1000 * 60 * 60 * 90});}
+        else{res.cookie('defaultExamGrade', ScGrade);}
+    } else if(cookieData && !ScGrade) { // 쿠키 적용
+        ScGrade = Number(cookieData)
+    }
+
+    if(!ScGrade) {
+        tableData = ''
+    } else {
+        const fs = require('fs')
+        if(!(ScGrade == 1 || ScGrade == 2 || ScGrade == 3)) {res.redirect('/exam/eval-list')}
+        tableData = fs.readFileSync(`${req.FileBaseRoot}/PageData/exam;midfinal/exam;dataGrade${ScGrade}.html`)
+    }
+
+    if(!ScGrade) {ScGrade == ''}
+
+    EndWithRespond(req, res, 'exam;list', [
+        {code: 'table_data', content: String(tableData)},
+        {code: 'grade', content: String(ScGrade)}
+    ])
 })
 
 module.exports = router;
